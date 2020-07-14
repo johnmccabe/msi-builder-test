7b18541703db0765ce3601fbf43da25975316b75
John McCabe <john@johnmccabe.net>
2020-07-14 19:47:11 +0100

    Strip down wix commands

--- package.sh
+++ package.sh
@@ -1,14 +1,10 @@
 #!/bin/bash -x
 
-choco install 7zip nuget.commandline
+choco install nuget.commandline
 nuget install WiX
 
-# Create zip archive
-7z a -tzip "./msi-builder-test.exe"
-
 # Create msi installer
-./WiX.*/tools/candle.exe -nologo -arch "x64" -ext WixUIExtension -ext WixUtilExtension -out \
+./WiX.*/tools/candle.exe -nologo -arch "x64" -out \
     "msi-builder-test.wixobj" "msi-builder-test.wxs"
-./WiX.*/tools/light.exe -nologo -ext WixUIExtension -ext WixUtilExtension -out \
-    "installer.msi" -sice:ICE61 -sice:ICE91 "msi-builder-test.wixobj"
-mv "installer.msi" "msi-builder-test-installer.msi"
+./WiX.*/tools/light.exe -nologo -out \
+    "msi-builder-test-installer.msi" "msi-builder-test.wixobj"
