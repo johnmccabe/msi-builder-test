4287d66fbe74d57713e469e724159f75f5c61b51
John McCabe <john@johnmccabe.net>
2020-07-14 18:37:25 +0100

    Rename installer

--- package.sh
+++ package.sh
@@ -11,4 +11,4 @@ nuget install WiX
     "msi-builder-test.wixobj" "msi-builder-test.wxs"
 ./WiX.*/tools/light.exe -nologo -ext WixUIExtension -ext WixUtilExtension -out \
     "installer.msi" -sice:ICE61 -sice:ICE91 "msi-builder-test.wixobj"
-mv "installer.msi" "msi-builder-test-windows-installer.msi"
+mv "installer.msi" "msi-builder-test-installer.msi"
